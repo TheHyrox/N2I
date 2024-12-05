0de8ad5929ff47f61c1d556f3aab623120706aa6
Guillaume Dodin <gdodin53@gmail.com>
2024-12-05 19:39:32 +0100

    email chiant

---
+++ resources/views/email-input.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Frustrating Email Input</title>
+</head>
+<body>
+<form>
+    <label for="emailInput">Enter your email:</label>
+    <input type="text" id="emailInput" oninput="handleInput(event)" placeholder="username@domain.com" />
+    <button type="submit">Submit</button>
+</form>
+
+<div class="rules">
+    <p>Rules:</p>
+    <ul>
+        <li>Start with a letter</li>
+        <li>Alternating letters and numbers</li>
+        <li>Use @ before domain, and domain in reverse order</li>
+    </ul>
+</div>
+
+<script src="{{ asset('../../public/js/email-input.js') }}"></script> <!-- Lien vers le fichier JavaScript -->
+</body>
+</html>
--- resources/views/navigation.blade.php
+++ resources/views/navigation.blade.php
@@ -10,6 +10,7 @@
                 <li><a href="{{ route('captcha') }}">Captcha</a></li>
                 <li><a href="#">Galerie</a></li>
                 <li><a href="#">Contact</a></li>
+                <li><a href="{{ route('email-input') }}">Email chiant</a></li>
             </ul>
         </nav>
     </header>
