8d0bd28326be515e243c0170c0f1d7fdcb3e1b25
Romain <romainthebault22100@gmail.com>
2024-12-06 04:43:47 +0100

    font

--- resources/views/email_input.blade.php
+++ resources/views/email_input.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Lexend:wght@400;700&display=swap" rel="stylesheet">
     <title>Frustrating Email Input</title>
     <style>
         :root{
@@ -14,7 +15,7 @@
         }
         body {
             margin: 0;
-            font-family: Arial, sans-serif;
+            font-family: Lexend, sans-serif;
             line-height: 1.6;
             color: #fff;
             background-color: var(--lightblue);
