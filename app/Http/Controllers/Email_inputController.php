3dcb949c66dfb933d80e1b201fa7c4772b21a17e
TheHyrox <thehyrox@gmail.com>
2024-12-06 04:52:11 +0100

    Remove 404 error handling for missing file in Email_inputController

--- app/Http/Controllers/Email_inputController.php
+++ app/Http/Controllers/Email_inputController.php
@@ -10,9 +10,6 @@ class Email_inputController extends Controller
     {
         // Read the file contents
         $filePath = public_path('VERY LONG TEXT.TXT');
-        if (!file_exists($filePath)) {
-            abort(404, 'File not found');
-        }
         $fileContents = file_get_contents($filePath);
 
         return view('email_input', ['fileContents' => $fileContents]);
