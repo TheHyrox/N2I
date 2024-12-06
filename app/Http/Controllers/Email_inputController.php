bc41b4381cc35d509d841ca273c37d69331b15ea
TheHyrox <thehyrox@gmail.com>
2024-12-06 04:56:01 +0100

    a

--- app/Http/Controllers/Email_inputController.php
+++ app/Http/Controllers/Email_inputController.php
@@ -9,7 +9,7 @@ class Email_inputController extends Controller
     public function index(Request $request)
     {
         // Read the file contents
-        $filePath ='VERY LONG TEXT.TXT';
+        $filePath ='/VERY LONG TEXT.TXT';
         $fileContents = file_get_contents($filePath);
 
         return view('email_input', ['fileContents' => $fileContents]);
